e5e0e8dcb06f52068c35c50e9abde5b892989be2
Robert Harężlak <harezlak.robert7@gmail.com>
2017-02-27 23:52:22 +0100

    added def create & params.requires

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,8 +1,24 @@
 class JobsController < ApplicationController
   def index
+    @jobs = Job.all
   end
 
   def new
     @job = Job.new
   end
+
+  def create
+    @job = Job.new(job_params)
+     if @job.save
+       redirect_to root_path
+     else
+       render "new"
+     end
+  end
+end
+
+private
+
+def job_params
+  params.require(:job).permit(:title, :company, :url)
 end
